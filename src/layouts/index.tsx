65299da1389b7498a4699bdcba64c60a59cbb6e8
bemayr <bernhard@mayr.io>
2019-03-11 09:18:26 +0100

    should fix link icon preview

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,8 +5,6 @@ import { StaticQuery, graphql } from 'gatsby'
 import 'modern-normalize'
 import '../styles/normalize'
 
-import logo from '../assets/logo.inverted.png'
-
 import LayoutRoot from '../components/LayoutRoot'
 import LayoutMain from '../components/LayoutMain'
 
@@ -44,12 +42,12 @@ const IndexLayout: React.SFC = ({ children }) => (
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="en" />
           <meta property="og:site_name" content={title} />
-          <meta property="og:image" content={logo} />
+          <meta property="og:image" content="/logo.social-media.png" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content={twitter} />
           <meta name="twitter:description" content={description} />
-          <meta name="twitter:image" content={logo} />
+          <meta name="twitter:image" content="/logo.social-media.png" />
         </Helmet>
         <LayoutMain>{children}</LayoutMain>
       </LayoutRoot>
