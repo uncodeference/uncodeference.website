79ba31ed77aa7262d4c2b7783a6bdd2703961089
bemayr <bernhard@mayr.io>
2019-03-11 09:29:53 +0100

    fixed open graph and twitter entries

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -41,11 +41,13 @@ const IndexLayout: React.SFC = ({ children }) => (
           <meta property="og:url" content={siteUrl} />
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="en" />
-          <meta property="og:site_name" content={title} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
           <meta property="og:image" content="/logo.social-media.png" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content={twitter} />
+          <meta name="twitter:title" content={title} />
           <meta name="twitter:description" content={description} />
           <meta name="twitter:image" content="/logo.social-media.png" />
         </Helmet>
