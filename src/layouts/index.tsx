21f6dcad36f96301473a09ffa68a924cda8f6b58
bemayr <bernhard@mayr.io>
2019-03-11 13:18:37 +0100

    fixed wrong image format again

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -43,7 +43,7 @@ const IndexLayout: React.SFC = ({ children }) => (
           <meta property="og:locale" content="en" />
           <meta property="og:title" content={title} />
           <meta property="og:description" content={description} />
-          <meta property="og:image" content={`${siteUrl}/large-card.social-media.png`} />
+          <meta property="og:image" content={`${siteUrl}/large-card.social-media.jpg`} />
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content={twitter} />
